00afcead715e09bb07529c5772fc3569f95ef8c5
L <18282658520@163.com>
2018-07-26 17:04:41 +0800

    merge

---
+++ yunjiagong/src/main/java/com/niule/yunjiagong/yunjiagong/controller/ComplaintController.java
@@ -0,0 +1,8 @@
+package com.niule.yunjiagong.yunjiagong.controller;
+
+/**
+ * @author haijun
+ * @create 2018 - 07 - 26 - 16:18
+ */
+public class ComplaintController {
+}
---
+++ yunjiagong/src/main/java/com/niule/yunjiagong/yunjiagong/service/ComplaintService.java
@@ -0,0 +1,8 @@
+package com.niule.yunjiagong.yunjiagong.service;
+
+/**
+ * @author haijun
+ * @create 2018 - 07 - 26 - 16:18
+ */
+public class ComplaintService {
+}
