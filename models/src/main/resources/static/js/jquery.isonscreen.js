e0c321cbad1bf93c99a646c76b76cc04767e0080
HJ <gaohaijun_2010@163.com>
2018-08-30 09:59:15 +0800

    change code

--- models/src/main/resources/static/js/jquery.isonscreen.js
+++ models/src/main/resources/static/js/jquery.isonscreen.js
@@ -1,15 +1,15 @@
 (function ($) {
 
-    $.fn.isOnScreen = function(x, y){
+    $.fn.isOnScreen = function (x, y) {
 
-        if(x == null || typeof x == 'undefined') x = 1;
-        if(y == null || typeof y == 'undefined') y = 1;
+        if (x == null || typeof x == 'undefined') x = 1;
+        if (y == null || typeof y == 'undefined') y = 1;
 
         var win = $(window);
 
         var viewport = {
-            top : win.scrollTop(),
-            left : win.scrollLeft()
+            top: win.scrollTop(),
+            left: win.scrollLeft()
         };
         viewport.right = viewport.left + win.width();
         viewport.bottom = viewport.top + win.height();
@@ -17,7 +17,7 @@
         var height = this.outerHeight();
         var width = this.outerWidth();
 
-        if(!width || !height){
+        if (!width || !height) {
             return false;
         }
 
@@ -27,15 +27,15 @@
 
         var visible = (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
 
-        if(!visible){
+        if (!visible) {
             return false;
         }
 
         var deltas = {
-            top : Math.min( 1, ( bounds.bottom - viewport.top ) / height),
-            bottom : Math.min(1, ( viewport.bottom - bounds.top ) / height),
-            left : Math.min(1, ( bounds.right - viewport.left ) / width),
-            right : Math.min(1, ( viewport.right - bounds.left ) / width)
+            top: Math.min(1, (bounds.bottom - viewport.top) / height),
+            bottom: Math.min(1, (viewport.bottom - bounds.top) / height),
+            left: Math.min(1, (bounds.right - viewport.left) / width),
+            right: Math.min(1, (viewport.right - bounds.left) / width)
         };
 
         return (deltas.left * deltas.right) >= x && (deltas.top * deltas.bottom) >= y;
